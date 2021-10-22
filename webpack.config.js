f2fed17c8e12388cf205f6c71c98fcfd272ad888
Luidjy PISTOL <luidjy.pistol@sully-group.fr>
2021-10-22 13:44:29 +0200

    :heavy_plus_sign: [JS] Initialisation de TailwindCSS

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -70,6 +70,9 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    // Enable PostCSS Support
+    .enablePostCssLoader()
 ;
 
 module.exports = Encore.getWebpackConfig();
